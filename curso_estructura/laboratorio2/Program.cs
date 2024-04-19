a46e4c5c802a133576fdfc6b7f1da6e5c041f41b
chris29cba <143229790+chris29cba@users.noreply.github.com>
2024-04-19 11:57:02 -0500

    laboratoyio2 #cambio

---
+++ curso_estructura/laboratorio2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See programa que sume los n primeros numeros 
+var n = 10;
+var suma = 0;
+for (var i = 1; i < n; i++)
+{
+    suma = suma + i;
+}
+
+Console.WriteLine("el resultaDO ES : " + suma.ToString());
